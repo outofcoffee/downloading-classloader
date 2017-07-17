03d02ba36516c575bede106ac20d54423346b100
Pete Cornish <outofcoffee@gmail.com>
2017-07-17 22:22:58 +0100

    Allow use of classloader without downloading.

--- src/main/kotlin/com/gatehill/dlcl/classloader/DownloadingClassLoader.kt
+++ src/main/kotlin/com/gatehill/dlcl/classloader/DownloadingClassLoader.kt
@@ -1,5 +1,6 @@
 package com.gatehill.dlcl.classloader
 
+import com.gatehill.dlcl.Downloader
 import com.gatehill.dlcl.mavenCentral
 import org.eclipse.aether.graph.Exclusion
 import java.net.URLClassLoader
@@ -14,10 +15,20 @@ open class DownloadingClassLoader(private val repoBaseDir: String,
                                   private val repositories: List<Pair<String, String>> = listOf(mavenCentral),
                                   parent: ClassLoader = getSystemClassLoader()) : URLClassLoader(emptyArray(), parent) {
 
+    fun load(root: String, excludes: List<Exclusion> = emptyList()) {
+        withDownloader(root, excludes) {
+            collectJars().forEach { addURL(it.file.toUri().toURL()) }
+        }
+    }
+
     fun fetch(root: String, excludes: List<Exclusion> = emptyList()) {
-        with(com.gatehill.dlcl.Downloader(repoBaseDir, root, excludes, repositories)) {
+        withDownloader(root, excludes) {
             download()
             collectJars().forEach { addURL(it.file.toUri().toURL()) }
         }
+    }
+
+    private fun withDownloader(root: String, excludes: List<Exclusion>, block: Downloader.() -> Unit) {
+        with(Downloader(repoBaseDir, root, excludes, repositories)) { block() }
     }
 }
